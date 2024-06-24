c51366d1a346e33199848f3e7bdb2f82284b5525
Chetalam <laura.limo@strathmore.edu>
2024-06-24 12:59:13 +0300

    area.html to php

---
+++ area.php
@@ -0,0 +1,16 @@
+<h1>Areas covered</h1>
+<?php include('header.php'); ?> 
+
+<a href="index.html">Home</a> | <a href="about.html">About Us</a> |
+    <a href="registration.html">Registration</a> | <a href="area.html">Area</a> |
+     <a href="projects.html">Projects</a> | <a href="design.html">Design</a> |
+      <a href="contact.html">Contact Us</a> | <a href="help.html">Help</a>
+ <h1> Where do we build?</h1>
+<p>Over the past 4 years we have grown a lot, and have ventured into many exciting projects. We have built houses in almost every part of 
+    the country and also on different terrain. So far, we have covered the surbaban area, the mountains, the hills, the beach and also the
+     city. We look forward to new adventures as we continue to build for you.<br></br>
+     <img src="images/Mid century modern.jpeg" alt="images" class="margin-right" width="35%">
+     <img src="images/Mediterranean.jpeg" alt="images" class="margin-left" width="35%">
+</p>
+
+<?php include ('footer.php'); ?>
