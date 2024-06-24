18ca75f424000afffe7fe801c2206d567e6a0cae
Chetalam <laura.limo@strathmore.edu>
2024-06-24 13:15:26 +0300

    help.html to php

---
+++ help.php
@@ -0,0 +1,19 @@
+<h1>How vcan we help you</h1>
+<?php include('header.php'); ?> 
+
+<a href="index.html">Home</a> | <a href="about.html">About Us</a> |
+    <a href="registration.html">Registration</a> | <a href="area.html">Area</a> |
+     <a href="projects.html">Projects</a> | <a href="design.html">Design</a> |
+      <a href="contact.html">Contact Us</a> | <a href="help.html">Help</a>
+ <h1> Do you need any help?</h1>
+<p>If you need any help, kindly ask our 24 hour customer service at LbLCustomer@gmail.com.<br></br>
+    Have a lovely day ahead!<br></br>
+    <img src="images/Colonial.jpeg" alt="images" class="margin-right" width="35%">
+</p>
+<footer>
+    <p>Author: L<br>
+    <p>&copy; 2024 LivingbyL. All rights reserved.</p>
+    <a href="LivingbyL: LbLCustomer@gmail.com">LbLCustomer@gmail.com</a></p>
+  </footer>
+
+<?php include ('footer.php'); ?>
