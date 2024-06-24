8689416a835e243582d022c93ab84eb467cbfe78
Chetalam <laura.limo@strathmore.edu>
2024-06-24 13:06:20 +0300

    contact.html to php

---
+++ contact.php
@@ -0,0 +1,17 @@
+<h1>Contacting us</h1>
+<?php include('header.php'); ?> 
+
+<a href="index.html">Home</a> | <a href="about.html">About Us</a> |
+    <a href="registration.html">Registration</a> | <a href="area.html">Area</a> |
+     <a href="projects.html">Projects</a> | <a href="design.html">Design</a> |
+      <a href="contact.html">Contact Us</a> | <a href="help.html">Help</a>
+ <h1> How can you get in contact with us?</h1>  
+<p> You can get in contact with us via email and phone provided below. You may also choose to visit us. <br></br>
+    Working hours are from 9am to 3pm.<br></br>
+    LivingbyLel@gmail.com<br></br>
+    +2540 00000000<br></br>
+    1st Western Avenue off Krish Street.<br></br>
+    <img src="images/Townhouse.jpeg" alt="images" class="margin-left" width="35%">
+</p>
+
+<?php include ('footer.php'); ?>
