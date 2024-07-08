8f1bafadc4a8a77dd904677ec226d9c8bc0ddbfd
Chetalam <laura.limo@strathmore.edu>
2024-07-08 14:30:22 +0300

    Introduced nav

---
+++ templates/nav.php
@@ -0,0 +1,4 @@
+<a href="index.php">Home</a> | <a href="about.php">About Us</a> |
+    <a href="registration.php">Registration</a> | <a href="area.php">Area</a> |
+     <a href="projects.php">Projects</a> | <a href="design.php">Design</a> |
+      <a href="contact.php">Contact Us</a> | <a href="help.php">Help</a>  
