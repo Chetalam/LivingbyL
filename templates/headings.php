f73d40e6c34d95a3f453ce1f1ff3a7f884504150
Chetalam <laura.limo@strathmore.edu>
2024-07-08 13:24:04 +0300

    Added a functionality

---
+++ templates/headings.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LivingbyL</title>
+    <link rel="stylesheet" href="css/style.css">
+    <style>
+        body{
+            background-color:darkkhaki;
+        }
+        .center{
+            display: block;
+            margin-right: auto;
+            margin-left: auto;
+        }
+        h1{
+            text-align: center;
+            color:saddlebrown;
+        }
+        p{
+            text-align:left;
+            color:black;
+        }
+
+        div{
+            margin:auto;
+            background-color:rgb(243, 235, 235);
+            text-align: center;
+        }
+        
+        </style>
+</head>
