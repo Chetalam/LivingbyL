9c43bd13beb260f1ec4c2c7ca156605b84fb7e73
Chetalam <laura.limo@strathmore.edu>
2024-07-10 21:31:33 +0300

    signin php

---
+++ templates/signin.php
@@ -0,0 +1,52 @@
+<?php include_once("templates/headings.php"); ?>
+<body>
+    <?php include_once("templates/nav.php"); ?>
+    <div class ="banner">
+
+
+    <div  class ="topnav-right">
+        <a class="btn btn-secondary dropdown-toggle" 
+           href="#" 
+           role="button" 
+           id="dropdownMenuLink"
+           data-bs-toggle="dropdown" 
+           aria-expanded="false">
+            =
+        </a>
+
+    <ul class="dropdown-menu" 
+                aria-labelledby="dropdownMenuLink">
+                <li><a class="dropdown-item" href="signin.html "  >
+                     signIn
+                    </a>
+                </li>
+                <li><a class="dropdown-item" href="signup.html">
+                        signUp
+                    </a>
+                </li>
+              
+            </ul>
+        </div>
+     </div>
+     <div class = banner>
+        <h1>SignIn</h1>
+         </div>
+
+           <div class = "row">
+             <div class = "content">
+                <form action=""autocomplete = "on">
+                   <!-- we are creating input type for name  -->
+                  <label >email:</label><br><br>
+                     <input type="name"  name="name" placeholder="name"><br><br>
+                       <!-- we are creating input type for password  -->
+                      <label >create a password</label><br>
+                         <input type="password"name="Create a password" placeholder=" password"><br><br>
+                           <!-- we are creating a an input for submit -->
+                           <input type="submit" value="signIn">
+
+                            <input type="button" value = "reset">
+
+
+                              </form>
+</body>
+</html>
