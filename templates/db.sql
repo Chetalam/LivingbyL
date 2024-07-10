a23e868c2c7422e82ed6a532c6471df350b22991
Chetalam <laura.limo@strathmore.edu>
2024-07-10 17:16:49 +0300

    registration table

--- templates/db.sql
+++ templates/db.sql
@@ -7,3 +7,14 @@ USE livingbyl;
 -- create table
 
 DROP TABLE IF EXISTS registration;
+
+DROP TABLE IF EXISTS registration;
+CREATE TABLE IF NOT EXISTS registration(
+    fullname varchar(50) NOT NULL DEFAULT '',
+    budget varchar(50) NOT NULL DEFAULT '',
+    password varchar(50) NOT  NULL DEFAULT '',
+    dateofbirth varchar(50) NOT NULL DEFAULT '',
+    description varchar(50) NOT NULL DEFAULT '',
+    PRIMARY KEY(fullname)
+
+);
