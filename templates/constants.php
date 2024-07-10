59a99b8b8906d5fc647f72446fd008c15ee987b9
Chetalam <laura.limo@strathmore.edu>
2024-07-10 14:59:31 +0300

    Constants

---
+++ templates/constants.php
@@ -0,0 +1,11 @@
+<?php
+// create PHP constant to enable connection to the MySQL database
+
+// create constants
+
+define("HOSTNAME", "localhost");
+define("HOSTUSER", "root");
+define("HOSTPASS", "  ");
+define("DBNAME", "LivingbyL");
+
+?>
