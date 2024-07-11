e8afcde52bcde94a8550ff7158c72cf15f39c255
Chetalam <laura.limo@strathmore.edu>
2024-07-11 13:08:08 +0300

    functionality

--- area.php
+++ area.php
@@ -1,5 +1,6 @@
 <h1>Areas covered</h1>
-<?php include('header.php'); ?> 
+<?php include_once ("templates/headings.php"); ?> 
+<?php include_once("templates/nav.php"); ?>
 
  <h1> Where do we build?</h1>
 <p>Over the past 4 years we have grown a lot, and have ventured into many exciting projects. We have built houses in almost every part of 
